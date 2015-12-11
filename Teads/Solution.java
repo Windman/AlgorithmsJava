35d367d8be59a99d8f165721e9d87d17ae3d32a7
Kilovatiy Maksim <kilovatymv@btf-consulting.ru>
2015-12-11 15:11:44 +0500

    Teads solved 90%

--- Teads/Solution.java
+++ Teads/Solution.java
@@ -6,11 +6,11 @@ import edu.princeton.cs.algs4.StdOut;
 import java.io.*;
 import java.math.*;
 
+//https://www.quora.com/Graph-Theory/What-is-the-most-efficient-algorithm-for-finding-the-center-of-a-tree
 //In this exercice, your mission consists in finding the minimal amount of hours it would take for a message 
 //to propagate across the entire network given to you as input
 class Solution {
-
-    //Don't copy to CG this main method
+   
 	public static void main(String args[]) {
         In in = new In(args[0]);
         int n = in.readInt();
@@ -23,64 +23,35 @@ class Solution {
             G.addEdge(v, w);
         }
         
-        int s = 1;
+        int c = G.findCenter();
         
-        BreadthFirstSearch bfs = new BreadthFirstSearch(G, s);
+        BreadthFirstSearch bfs = new BreadthFirstSearch(G,c);
         
-        for (int v = 0; v < G.V(); v++) {
-            if (bfs.hasPathTo(v)) {
-                StdOut.printf("%d to %d (%d):  ", s, v, bfs.distTo(v));
-                for (int x : bfs.pathTo(v)) {
-                    if (x == s) StdOut.print(x);
-                    else        StdOut.print("-" + x);
-                }
-                StdOut.println();
+        int max = bfs.distances()[0];
+        for (int i = 1; i < bfs.distances().length; i++) {
+            if (bfs.distances()[i] != Integer.MAX_VALUE && bfs.distances()[i] > max) {
+              max = bfs.distances()[i];
             }
-
-            else {
-                StdOut.printf("%d to %d (-):  not connected\n", s, v);
-            }
-
         }
         
-        System.out.println(G);
+        //System.out.println(G);
+        System.out.println("Center: "+c);   
+        System.out.println(max);    
     }
     
-    static class BreadthFirstSearch {
+	static class BreadthFirstSearch {
         private static final int INFINITY = Integer.MAX_VALUE;
-        private boolean[] marked;  // marked[v] = is there an s-v path
-        private int[] edgeTo;      // edgeTo[v] = previous edge on shortest s-v path
-        private int[] distTo;      // distTo[v] = number of edges shortest s-v path
-
-        /**
-         * Computes the shortest path between the source vertex <tt>s</tt>
-         * and every other vertex in the graph <tt>G</tt>.
-         * @param G the graph
-         * @param s the source vertex
-         */
+        private boolean[] marked;  
+        private int[] edgeTo;      
+        private int[] distTo;      
+        
         public BreadthFirstSearch(Graph G, int s) {
             marked = new boolean[G.V()];
             distTo = new int[G.V()];
             edgeTo = new int[G.V()];
             bfs(G, s);
         }
-
-        /**
-         * Computes the shortest path between any one of the source vertices in <tt>sources</tt>
-         * and every other vertex in graph <tt>G</tt>.
-         * @param G the graph
-         * @param sources the source vertices
-         */
-        public BreadthFirstSearch(Graph G, Iterable<Integer> sources) {
-            marked = new boolean[G.V()];
-            distTo = new int[G.V()];
-            edgeTo = new int[G.V()];
-            for (int v = 0; v < G.V(); v++)
-                distTo[v] = INFINITY;
-            bfs(G, sources);
-        }
-
-        // breadth-first search from a single source
+        
         private void bfs(Graph G, int s) {
             Queue<Integer> q = new Queue<Integer>();
             for (int v = 0; v < G.V(); v++)
@@ -101,64 +72,13 @@ class Solution {
                 }
             }
         }
-
-        // breadth-first search from multiple sources
-        private void bfs(Graph G, Iterable<Integer> sources) {
-            Queue<Integer> q = new Queue<Integer>();
-            for (int s : sources) {
-                marked[s] = true;
-                distTo[s] = 0;
-                q.enqueue(s);
-            }
-            while (!q.isEmpty()) {
-                int v = q.dequeue();
-                for (int w : G.adj(v)) {
-                    if (!marked[w]) {
-                        edgeTo[w] = v;
-                        distTo[w] = distTo[v] + 1;
-                        marked[w] = true;
-                        q.enqueue(w);
-                    }
-                }
-            }
-        }
-
-        /**
-         * Is there a path between the source vertex <tt>s</tt> (or sources) and vertex <tt>v</tt>?
-         * @param v the vertex
-         * @return <tt>true</tt> if there is a path, and <tt>false</tt> otherwise
-         */
-        public boolean hasPathTo(int v) {
-            return marked[v];
-        }
-
-        /**
-         * Returns the number of edges in a shortest path between the source vertex <tt>s</tt>
-         * (or sources) and vertex <tt>v</tt>?
-         * @param v the vertex
-         * @return the number of edges in a shortest path
-         */
-        public int distTo(int v) {
-            return distTo[v];
-        }
-
-        /**
-         * Returns a shortest path between the source vertex <tt>s</tt> (or sources)
-         * and <tt>v</tt>, or <tt>null</tt> if no such path.
-         * @param v the vertex
-         * @return the sequence of vertices on a shortest path, as an Iterable
-         */
-        public Iterable<Integer> pathTo(int v) {
-            if (!hasPathTo(v)) return null;
-            Stack<Integer> path = new Stack<Integer>();
-            int x;
-            for (x = v; distTo[x] != 0; x = edgeTo[x])
-                path.push(x);
-            path.push(x);
-            return path;
+               
+        public int[] distances() {
+            return distTo;
         }
     }
-    static class Queue<Item> {
+    
+	static class Queue<Item> {
         
     	private int N;               
         private Node<Item> first;    
@@ -179,6 +99,10 @@ class Solution {
             return first == null;
         }
         
+        public int size() {
+        	return N;
+        }
+        
         public void enqueue(Item item) {
             Node<Item> oldlast = last;
             last = new Node<Item>();
@@ -197,21 +121,38 @@ class Solution {
             if (isEmpty()) last = null;   
             return item;
         }
+        
+        public String toString() {
+            StringBuilder s = new StringBuilder();
+            for (Node x = first; x != null; x = x.next)
+            {
+            	s.append(x.item);
+            	s.append(" ");
+            }
+            return s.toString();
+        }
     }
-    static class Graph
+    
+	static class Graph
     {
     	public int V;
         public int E;
         public List<Integer>[] adj;
+        private Integer[] marked;
+        private int adjLength;
         
         public Graph(int n) {
             if (n < 0) throw new IllegalArgumentException("vertices");
             this.V = n + 1;
             this.E = 0;
+            marked = new Integer[V];
             adj = (ArrayList<Integer>[]) new ArrayList[V];
             for (int v = 0; v < V; v++) {
                 adj[v] = new ArrayList<Integer>();
+                marked[v] = 0;
             }
+            
+            adjLength = adj.length;
         }
     	
         public Iterable<Integer> adj(int v) {
@@ -224,8 +165,59 @@ class Solution {
         
     	public void addEdge(int v, int w){
     		E++;
+    		if (v > adjLength || w > adjLength)
+    			resize(2*adjLength);
             adj[v].add(w);
             adj[w].add(v);
+    	}
+    	
+    	@SuppressWarnings("unchecked")
+		private void resize(int c) {
+    		List<Integer>[] a;
+    		a = (ArrayList<Integer>[]) new ArrayList[c];
+    		Integer[] m = new Integer[c];
+    		
+    		for (int v = 0; v < c; v++) {
+                a[v] = new ArrayList<Integer>();
+                m[v] = 0;
+            }
+    		
+    		for (int i = 0; i < V; i++) {
+                a[i] = adj[i];
+            }
+            adj = a;
+            
+            for (int i = 0; i < V; i++) {
+                m[i] = marked[i];
+            }
+            marked = m;
+            
+            adjLength = c;
+            V = c;
+        }   
+    	
+    	public int findCenter()
+    	{
+    		Queue<Integer> q = new Queue<Integer>();
+    		for (int i = 1; i < adj.length; i++) {
+				if (adj[i].size() == 1) {
+					q.enqueue(i);
+					marked[i] = 1;
+				}
+			}
+    		
+    		while(q.size() > 1 ) {
+    			Integer e = q.dequeue();
+    			    				
+    			for(int v: adj[e] ) {
+    				if (marked[v] == 0) {
+    					q.enqueue(v);
+    					marked[v] = 1;
+    				}
+    			}
+    		}
+    		
+    		return q.dequeue();
     	}
     	
     	public String toString() {
