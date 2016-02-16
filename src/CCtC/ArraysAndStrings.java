4beff305ed2f38f7bbdfeab2e3c453f5a5761882
Kilovatiy Maksim <kilovatymv@btf-consulting.ru>
2016-02-16 12:51:11 +0500

    CCtC 1.4 Complete

---
+++ src/CCtC/ArraysAndStrings.java
@@ -0,0 +1,59 @@
+package CCtC;
+
+import java.util.*;
+
+public class ArraysAndStrings {
+
+	public static void main(String[] args) {
+				
+	}
+	
+	public void TransformString_1_4()
+	{
+		String input = "Mr John Smith Black      ";
+		char[] str = input.toCharArray();
+		
+		ArrayList<Integer> aux = new ArrayList<Integer>();
+		aux.add(-1);
+		int lastPos = -1;
+		
+		//Preparation
+		for (int i = 0; i < str.length; i++) {
+			if(i < str.length -1 && str[i] == ' ' && str[i+1] != ' '){
+				aux.add(i);
+			}
+			else if(i < str.length -2 && str[i] != ' ' && str[i+1] == ' '&& str[i+2] == ' ')
+				lastPos = i;
+		}		
+		
+		int[] p = convertIntegers(aux);
+		//End of preparation
+		
+		for (int i = p.length-1; i >= 1; i--) {
+			int m = i * 2;
+			
+			for (int j = lastPos; j > p[i]; j--) {
+				str[j+m] = str[j];
+			}
+			str[p[i] + m]   = '0';
+			str[p[i]-1 + m] = '2';
+			str[p[i]-2 + m] = '%';
+			lastPos = p[i] -1;
+		}
+		
+		for (int i = 0; i < str.length; i++) {
+			System.out.print(str[i]);
+		}
+	}
+	
+	public static int[] convertIntegers(ArrayList<Integer> integers)
+	{
+	    int[] ret = new int[integers.size()];
+	    Iterator<Integer> iterator = integers.iterator();
+	    for (int i = 0; i < ret.length; i++)
+	    {
+	        ret[i] = iterator.next().intValue();
+	    }
+	    return ret;
+	}
+}
--- src/alg/Program.java
+++ src/alg/Program.java
@@ -1,125 +1,14 @@
 package alg;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.NoSuchElementException;
 
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Integer R = 25;
-		Integer L = 10;
-		
-		ArrayList<Integer> seq = new ArrayList<Integer>();
-		Queue<Integer> q = new Queue<Integer>();
-		q.enqueue(25);
-		
-		if (L == 1)
-			seq.add(R);
-		else
-			for (int i = 1; i < L; i++) {
-				seq = Generate(q);
-				q = new Queue<Integer>();
-				for (Integer e: seq) {
-					q.enqueue(e);
-				}
-			}
-				
-		int c = 1;
-		for(int i:seq) {
-			System.out.print(i);
-			if (c != seq.size()) {
-				System.out.print(" ");
-				c++;
-			}
-		}
-	}
-	
-	public static ArrayList<Integer> Generate(Queue<Integer> seq) {
-		ArrayList<Integer> newSeq = new ArrayList<Integer>();
-		Integer value  = 0;
-		Integer q = 1;
-		Integer[] marked = new Integer[100];
-		while(!seq.isEmpty()) {
-			value = seq.dequeue();
-			if (marked[value] == null) { //not marked
-				//PreviousMarked
-				AddElement(marked, newSeq, q);
-				marked[value] = 1;
-				q = 1;
-			}
-			else {
-				q++;
-			}
-		}
-		AddElement(marked, newSeq, q);
-		return newSeq;
-	}
-	
-	static void AddElement(Integer[] marked, ArrayList<Integer> newSeq, Integer q){
-		for (int i = 0; i < marked.length; i++) {
-			if (marked[i] != null) {
-				newSeq.add(q);
-				newSeq.add(i);
-				marked[i] = null;
-			}
-		} 
-	}
-	
-	static class Queue<Item> {
-	    
-		private int N;               
-	    private Node<Item> first;    
-	    private Node<Item> last;     
-	    
-	    private static class Node<Item> {
-	        private Item item;
-	        private Node<Item> next;
-	    }
-	    
-	    public Queue() {
-	        first = null;
-	        last  = null;
-	        N = 0;
-	    }
-	    
-	    public boolean isEmpty() {
-	        return first == null;
-	    }
-	    
-	    public int size() {
-	    	return N;
-	    }
-	    
-	    public void enqueue(Item item) {
-	        Node<Item> oldlast = last;
-	        last = new Node<Item>();
-	        last.item = item;
-	        last.next = null;
-	        if (isEmpty()) first = last;
-	        else           oldlast.next = last;
-	        N++;
-	    }
-	    
-	    public Item dequeue() {
-	        if (isEmpty()) throw new NoSuchElementException("underflow");
-	        Item item = first.item;
-	        first = first.next;
-	        N--;
-	        if (isEmpty()) last = null;   
-	        return item;
-	    }
-	    
-	    public String toString() {
-	        StringBuilder s = new StringBuilder();
-	        for (Node x = first; x != null; x = x.next)
-	        {
-	        	s.append(x.item);
-	        	s.append(" ");
-	        }
-	        return s.toString();
-	    }
+
 	}
 }
 
